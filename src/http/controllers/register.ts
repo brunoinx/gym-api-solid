0d79a5990181de47d6f6fcc24083b163c2b280f9
Bruno Sousa <bruno.sousa@profiz.com.br>
2025-09-24 12:12:02 -0300

    feat: :sparkles: adiciona tratamento de erro para usuário já existente no registro

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,9 +1,11 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import z from 'zod';
 
-import { registerUseCase } from 'use-cases/register';
+import { RegisterUseCase } from 'use-cases/register';
+import { PrismaUsersRepository } from 'repositories/prisma/users.repository';
+import { UserAlreadyExistsError } from 'use-cases/errors/user-already-exists';
 
-export async function register(request: FastifyRequest, reply: FastifyReply) {
+export async function registerController(request: FastifyRequest, reply: FastifyReply) {
   const registerUserSchema = z.object({
     name: z.string(),
     email: z.email(),
@@ -13,9 +15,16 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerUserSchema.parse(request.body);
 
   try {
-    await registerUseCase({ name, email, password });
-  } catch {
-    return reply.status(409).send();
+    const usersRepository = new PrismaUsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
+
+    await registerUseCase.execute({ name, email, password });
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message });
+    }
+
+    throw error;
   }
 
   return reply.status(201).send();
