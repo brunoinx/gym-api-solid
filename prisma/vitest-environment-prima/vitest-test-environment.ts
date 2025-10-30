7e3e9c31205f0a96b48f64830e9571d9c6cb4f60
Bruno Sousa <bruno.sousa@profiz.com.br>
2025-10-30 16:11:23 -0300

    chore: :wrench: install supertest and config test enviroment for e2e tests

--- prisma/vitest-environment-prima/vitest-test-environment.ts
+++ prisma/vitest-environment-prima/vitest-test-environment.ts
@@ -3,7 +3,7 @@ import path from 'node:path';
 import { execSync } from 'node:child_process';
 import { ulid } from 'ulidx';
 import type { Environment } from 'vitest/environments';
-import { prisma } from 'lib/prisma';
+import { prisma } from '../../src/lib/prisma';
 
 export default <Environment>{
   name: 'prisma',
@@ -16,7 +16,7 @@ export default <Environment>{
 
     process.env.DATABASE_URL = databaseUrl;
 
-    execSync('npx prisma migrate deploy', { stdio: 'inherit' });
+    execSync('npx prisma migrate deploy');
 
     return {
       dbPath,
